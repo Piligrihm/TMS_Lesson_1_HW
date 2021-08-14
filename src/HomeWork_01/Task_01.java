b338e47dd129d5d304e678aba124e811e98cbe51
Piligrihm <71782601+Piligrihm@users.noreply.github.com>
2021-08-14 17:16:23 +0300

    Add files via upload

---
+++ src/HomeWork_01/Task_01.java
@@ -0,0 +1,9 @@
+package HomeWork_01;
+
+import java.util.Scanner;
+
+public class Task_01 {
+    public static void main(String[] args) {
+        System.out.println("Hello, team");
+    }
+}
---
+++ src/HomeWork_01/Task_02.java
@@ -0,0 +1,15 @@
+package HomeWork_01;
+
+import java.util.Scanner;
+
+public class Task_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, type the name...");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+
+    }
+}
+
